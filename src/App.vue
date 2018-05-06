a6ea852be4822cbf91315961a22a52c3f6d7cafa
SimonSiefke <simon.siefke@gmail.com>
2018-05-06 15:23:56 +0200

    remove clean urls

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home Page 123</router-link> |
+      <router-link to="/">Home Page 12345</router-link> |
       <router-link to="/about">About this page</router-link>
     </div>
     <router-view/>
