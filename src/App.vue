fbd936bfc94bf807ac1c160df72c682d1d669a29
SimonSiefke <simon.siefke@gmail.com>
2018-05-30 09:41:29 +0200

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home Page 12345</router-link> |
+      <router-link to="/">Home Page 678</router-link> |
       <router-link to="/about">About this page</router-link>
     </div>
     <router-view/>
