d5d53afe49881317c0963d2c3a4d02978fe0dbee
SimonSiefke <simon.siefke@gmail.com>
2018-06-02 10:48:08 +0200

    add scripts back in

--- travis/run_on_pull_requests.sh
+++ travis/run_on_pull_requests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "deploying to staging and test lighthouse score"
-# firebase deploy --token $FIREBASE_TOKEN --project staging
-# npm run lh $STAGING_URL
+firebase deploy --token $FIREBASE_TOKEN --project staging
+npm run lh $STAGING_URL
 exit 0
